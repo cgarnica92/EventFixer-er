4687114d86f18349b31e0faf5e8ceaaecdb69a8e
cgarnica92 <c_garnica92@yahoo.com>
2022-12-13 20:59:11 -0500

    app functionality and collapse menu added

---
+++ Develop/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    //Save Button
+    $('.saveBtn').on('click', function() {
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        localStorage.setItem( time, value );
+    });
+
+    //Hour Updater  
+    function hourUpdater() {
+        var currentHour = moment().hours();
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+            if ( blockHour < currentHour ) {
+                $(this).addClass('past');
+            } else if ( blockHour === currentHour ) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    hourUpdater();
+
+    //Interval for Update Check
+
+    var interval = setInterval(hourUpdater, 15000);
+
+    //Load Saved Data
+
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+    //Display Current Day
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+});
